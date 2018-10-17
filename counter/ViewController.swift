5a2e1e7c3c701f3786e36e1e38c42602673823cf
20161104574 <a20161104574@admindeiMac-12.local>
2018-10-17 11:14:20 +0800

    s

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func number8(_ sender: Any) {
+    }
+    @IBAction func number9(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
